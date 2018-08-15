679ad4014182cd7237f380142255daf8af05f877
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2018-08-15 13:30:08 +0200

    include bootstrap.php from local/Config dir

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * This file is loaded automatically by the app/webroot/index.php file after core.php
  *
- * This file should load/create any application wide configuration settings, such as 
+ * This file should load/create any application wide configuration settings, such as
  * Caching, Logging, loading additional configuration files.
  *
  * You should also use this file to include any files that provide global functions/constants
@@ -121,3 +121,9 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+
+// Cake expects bootstrap.php to hold logging configuations. We allow adding loggers from
+// LOCAL directory so it's easier to migrate across versions.
+// See issue CO-1643
+include(LOCAL . 'Config' . DS . 'bootstrap.php');
+
